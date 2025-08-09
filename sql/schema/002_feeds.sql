e65fd9462e612ba8fd7bc6cbf482fe7a5881e7bc
Bigesto <43073969+Bigesto@users.noreply.github.com>
2025-08-09 10:09:34 +0200

    Update 002_feeds.sql

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -14,7 +14,3 @@ CREATE TABLE feeds(
 
 -- +goose Down
 DROP TABLE feeds;
-
--- Connection string : psql "postgres://postgres:postgres@localhost:5432/gator"
--- Starts database : sudo service postgresql start
--- UP Database : goose -dir ./sql/schema postgres postgres://postgres:postgres@localhost:5432/gator up
